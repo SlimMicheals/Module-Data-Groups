057bd9587bd418faa6a94a8a68e1945f15b5cd36
SlimMicheals <slimmicheals77@gmail.com>
2026-01-28 12:17:25 +0100

    Add tally function and tests to count item frequency

--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -1,34 +1,22 @@
 const tally = require("./tally.js");
 
-/**
- * tally array
- *
- * In this task, you'll need to implement a function called tally
- * that will take a list of items and count the frequency of each item
- * in an array
- *
- * For example:
- *
- * tally(['a']), target output: { a: 1 }
- * tally(['a', 'a', 'a']), target output: { a: 3 }
- * tally(['a', 'a', 'b', 'c']), target output: { a : 2, b: 1, c: 1 }
- */
+test("returns empty object for empty array", () => {
+  expect(tally([])).toEqual({});
+});
 
-// Acceptance criteria:
+test("counts single item", () => {
+  expect(tally(["a"])).toEqual({ a: 1 });
+});
 
-// Given a function called tally
-// When passed an array of items
-// Then it should return an object containing the count for each unique item
+test("counts duplicate items", () => {
+  expect(tally(["a", "a", "b", "c"])).toEqual({
+    a: 2,
+    b: 1,
+    c: 1,
+  });
+});
 
-// Given an empty array
-// When passed to tally
-// Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
+test("throws error for invalid input", () => {
+  expect(() => tally("abc")).toThrow();
+});
 
-// Given an array with duplicate items
-// When passed to tally
-// Then it should return counts for each unique item
-
-// Given an invalid input like a string
-// When passed to tally
-// Then it should throw an error
