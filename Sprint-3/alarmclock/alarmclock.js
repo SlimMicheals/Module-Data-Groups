a69c3d62fa737ff5eda77ebd3bdec0133f76af6a
SlimMicheals <slimmicheals77@gmail.com>
2026-02-04 18:45:44 +0100

    feat: complete alarm clock countdown logic and pass all tests

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,5 +1,38 @@
-function setAlarm() {}
+let timer = null;
+let totalSeconds = 0;
 
+function setAlarm() {
+  clearInterval(timer);
+
+  const input = document.getElementById("alarmSet");
+  const heading = document.getElementById("timeRemaining");
+
+ totalSeconds = Number(input.value);
+
+ function updateHeading() {
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+
+   heading.innerText = `Time Remaining: ${minutes}:${seconds}`;
+  }
+
+
+  updateHeading();
+
+  timer = setInterval(() => {
+    totalSeconds--;
+
+    updateHeading();
+
+    if (totalSeconds <= 0) {
+      clearInterval(timer);
+      playAlarm();
+    }
+  }, 1000);
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
@@ -23,3 +56,4 @@ function pauseAlarm() {
 }
 
 window.onload = setup;
+
