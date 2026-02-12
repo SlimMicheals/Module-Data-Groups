164821b8d0277f1c1262669c5b0d9e2b79f622ab
SlimMicheals <slimmicheals77@gmail.com>
2026-02-12 19:58:45 +0100

    Complete reading list app with images and read/unread styling

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -21,3 +21,35 @@ const books = [
   },
 ];
 
+  function readingList(booksArray) {
+  const bookList = document.getElementById("reading-list");
+  bookList.innerHTML = "";
+
+  booksArray.forEach((book) => {
+    const li = document.createElement("li");
+    li.style.backgroundColor = book.alreadyRead ? "green" : "red";
+
+    const img = document.createElement("img");
+    img.src = book.bookCoverImage;
+
+    const title = document.createElement("h2");
+    title.innerText = book.title;
+
+    const author = document.createElement("p");
+    author.innerText = book.author;
+
+    li.appendChild(img);
+    li.appendChild(title);
+    li.appendChild(author);
+
+    bookList.appendChild(li);
+  });
+}
+
+readingList(books);
+
+
+if (typeof module !== "undefined") {
+  module.exports = { readingList };
+}
+
